55157edcefafc39a3fae2cd7f8ad6d2002bd3d04
shubhamrawat0224 <66406374+shubhamrawat0224@users.noreply.github.com>
2025-04-28 17:31:46 +0530

    Revert "Feedback"

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-/**
- * ISC License
- * Copyright (c) 2024 Airtribe
- */
-
-const express = require("express");
-const route = require("./routers/route");
+const express = require('express');
 const app = express();
 const port = 3000;
-const loggerMiddleware = require("./middleware/logger");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(loggerMiddleware);
-app.use("/api/v1/tasks", route);
 
 app.listen(port, (err) => {
-  if (err) {
-    return console.log("Something bad happened", err);
-  }
-  console.log(`Server is listening on ${port}`);
+    if (err) {
+        return console.log('Something bad happened', err);
+    }
+    console.log(`Server is listening on ${port}`);
 });
+
+
 
 module.exports = app;
